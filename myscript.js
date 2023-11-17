3495b5089fae180e36eb8b0a951b51c20d760263
AlessandroBilli <skillmanbungo@gmail.com>
2023-11-17 04:08:28 +0100

    feat: celle rosse

--- myscript.js
+++ myscript.js
@@ -2,8 +2,11 @@
 
 let play = document.getElementById('playButton');
 let clickedOnce = false;
-let grigliaDiv = document.querySelector('.griglia'); 
+let grigliaDiv = document.querySelector('.griglia');
+let reset = document.getElementById('resetButton');
 
+
+reset.addEventListener('click', reset);
 play.addEventListener('click', function generaCelle() {
     if (!clickedOnce) {
         for (let i = 0; i < 100; i++) {
@@ -12,18 +15,28 @@ play.addEventListener('click', function generaCelle() {
             cella.textContent = i + 1;
             grigliaDiv.appendChild(cella);
         }
+
+        let celle = document.querySelectorAll('.cella');
+
+        for (let i = 0; i < 16; i++) {
+            celle[i].classList.remove('cella');
+            celle[i].classList.add('cellarossa');
+        }
+
         clickedOnce = true;
 
-        let celleEffetto = document.querySelectorAll('.cella');
+        let celleEffetto = document.querySelectorAll('.cella, .cellarossa');
 
         celleEffetto.forEach(cella => {
-          cella.addEventListener('click', function() {
-            console.log(cella.textContent);
+            cella.addEventListener('click', function () {
+                console.log(cella.textContent);
 
-            if ('click') {
-                cella.style.backgroundColor= "#007fff";
-            }
-          });
+                if (cella.classList.contains('cella')) {
+                    cella.style.backgroundColor = "#007fff";
+                } else if (cella.classList.contains('cellarossa')) {
+                    cella.style.backgroundColor = "#FF0000";
+                }
+            });
         });
 
     } else {
@@ -41,5 +54,4 @@ play.addEventListener('click', function generaCelle() {
 
 
 
-  
 
